69f157a57e18550bc6c48dae896b2d1df1ff52a8
Kevin Li <hoi.li@unswalumni.com>
2017-04-10 11:03:57 +1000

    creating webcam access via javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+
+  var camera = new JpegCamera("#camera");
+
+  // var snapshot = camera.capture();
+  //
+  // snapshot.show(); // Display the snapshot
+  //
+  // snapshot.upload({api_url: "/upload_image"}).done(function(response) {
+  //   response_container.innerHTML = response;
+  //   this.discard(); // discard snapshot and show video stream again
+  // }).fail(function(status_code, error_message, response) {
+  //   alert("Upload failed with status " + status_code);
+  // });
+
+})
