17f27f5fa0d2bbcf6010eebc7716fcd8f3583bc8
Petr77 <Petr77@github.com>
2017-04-10 10:33:13 +1000

    seed file done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-u1 = User.create name: "Jarred", image_id: 1, email: "aaa@aaa.com", admin: false
+u1 = User.create name: "Jarred", image_id: 1, email: "aaa@aaa.com", admin: true
 u2 = User.create name: "Kevin", image_id: 2, email: "bbb@bbb.com", admin: false
-u1 = User.create name: "Jarred", image_id: 3, email: "ccc@ccc.com", admin: false
+u3 = User.create name: "Jarred", image_id: 3, email: "ccc@ccc.com", admin: false
 
-i1 = Image.create user_id: 1, playlist_id: 1, img_src: 
+i1 = Image.create user_id: 1, playlist_id: 1, img_src: "aaasg123ty1"
+i2 = Image.create user_id: 2, playlist_id: 2, img_src: "bbb1ghg412"
+i3 = Image.create user_id: 3, playlist_id: 3, img_src: "cccghj2342"
+
+p1 = Playlist.create image_id: 1, playlist_url: "ghj34gh234"
+p2 = Playlist.create image_id: 2, playlist_url: "gasjgh234"
+p3 = Playlist.create image_id: 3, playlist_url: "ghjsdfgsdgh234"
