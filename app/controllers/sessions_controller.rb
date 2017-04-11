f6e18be153eb1036eddf5e07fce442088a71a27f
jaredcolby <jaredkcolby@gmail.com>
2017-04-11 15:19:24 +1000

    merged

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user.present? && user.authenticate(params[:image_id])
+      user.id = session[:user_id]
+      redirect_to root_path
+      flash[:success] = "You have successfully signed in."
+    else
+      redirect_to signin_path
+      flash[:error] = "Something went wrong. Please try again."
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+    flash[:success] = "You have successfully signed out."
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :images
   has_many :playlists, through: :images
+  validates_uniqueness_of :email, :case_sensitive => false, :with => /.+@.+\..+/i
+  validates_uniqueness_of :name, :image_id
 end
